4ac29755cc673fb515e1903efc3ffd8672f01c65
13ye <2559877637@qq.com>
2022-02-10 17:38:39 +0800

    fix next export bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ export default function HomePage() {
       <main>
         <section className='bg-white'>
           <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
-            {//<Vercel className='text-5xl' />}
+            {/*<Vercel className='text-5xl' />*/}
             <h1 className='mt-4'>
               Next.js + Tailwind CSS + TypeScript Starter
             </h1>
