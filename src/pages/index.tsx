b1ff0ed3fb5f6b0bfb4b9663e699278e71934f68
13ye <2559877637@qq.com>
2022-02-10 17:36:33 +0800

    fix next export bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,8 @@ import Seo from '@/components/Seo';
  * You can override the next-env if the type is important to you
  * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
  */
-import Vercel from '~/svg/Vercel.svg';
+// exception here because of image
+//import Vercel from '~/svg/Vercel.svg';
 
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
 // Before you begin editing, follow all comments with `STARTERCONF`,
@@ -29,7 +30,7 @@ export default function HomePage() {
       <main>
         <section className='bg-white'>
           <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
-            <Vercel className='text-5xl' />
+            {//<Vercel className='text-5xl' />}
             <h1 className='mt-4'>
               Next.js + Tailwind CSS + TypeScript Starter
             </h1>
